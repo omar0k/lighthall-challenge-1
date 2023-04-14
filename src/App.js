97c11fdf373c80d5d138546790752069d9654f7d
omark01 <omark012000@gmail.com>
2023-04-14 03:43:04 -0400

    completed challenge

--- src/App.js
+++ src/App.js
@@ -1,23 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useEffect, useState } from "react";
 
 function App() {
+  const getInitialCount = () => {
+    const storedCount = localStorage.getItem("count");
+    return storedCount ? JSON.parse(storedCount) : 0;
+  };
+  const [count, setCount] = useState(getInitialCount());
+  useEffect(() => {
+    localStorage.setItem("count", JSON.stringify(count));
+  });
+  const incrementCount = () => {
+    setCount(count + 1);
+  };
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <button className="btn" onClick={incrementCount}>
+        Click me!
+      </button>
+      <h2 className="header">
+        You clicked <span style={{ color: "red" }}>{count}</span> times.
+      </h2>
     </div>
   );
 }
